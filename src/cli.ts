84f44bbf2287be74fa43e87db4d88d77b7173323
Asaf Korem <asaf.korem@gmail.com>
2024-08-17 16:17:13 +0300

    feature: prettify CLI logs.

--- src/cli.ts
+++ src/cli.ts
@@ -1,22 +1,50 @@
 import { analyzeRepository } from './analyzeRepository';
 import { AnalysisResult } from './types';
-import {promises as fs} from "fs";
-import {generateJSONFiles} from "./jsonGenerator";
+import { promises as fs } from "fs";
+import { generateJSONFiles } from "./jsonGenerator";
+
+const colors = {
+    green: "\x1b[32m",
+};
+
+function spinnerLog(start: string, end: string) {
+    const spinner = ['📁', '🔍', '🔬', '🧪', '📈', '📉', '📅', '🔧', '📊'];
+    let i = 0;
+    let currentMessage = '';
+
+    const intervalId = setInterval(() => {
+        process.stdout.write(`\r${' '.repeat(currentMessage.length)}`);
+
+        currentMessage = `${spinner[i]} ${start}`;
+        process.stdout.write(`\r${currentMessage}`);
+
+        i = (i + 1) % spinner.length;
+    }, 100);
+
+    return () => {
+        clearInterval(intervalId);
+        process.stdout.write(`\r${' '.repeat(currentMessage.length)}`);
+
+        process.stdout.write(`\r✅ \x1b[32m${end}\x1b[0m\n`);
+    };
+}
 
 export async function runAnalysis(repoPath: string, artifactsPath: string) {
     try {
-        console.log(`Analyzing repository: ${repoPath}...`);
+        const analysisCompletionLog = spinnerLog(
+            'Analyzing repository...',
+            'Repository analyzed successfully.'
+        );
         const analysisResult: AnalysisResult = await analyzeRepository(repoPath);
+        analysisCompletionLog();
 
         console.log(`Artifacts will be stored in: ${artifactsPath}`);
         await fs.mkdir(artifactsPath, { recursive: true });
         await generateJSONFiles(analysisResult, artifactsPath);
 
-        console.log('Analysis completed successfully.');
-
         // TODO: load dashboard and open it in the browser
     } catch (error) {
-        console.error('An error occurred during analysis:', error);
+        console.error(error);
         process.exit(1);
     }
 }
