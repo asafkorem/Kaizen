106c84e481e9f99df2ae3d670f4f724e81941741
Asaf Korem <asaf.korem@gmail.com>
2024-08-18 11:03:10 +0300

    Update to version v1.1.20

--- Formula/code-kaizen.rb
+++ Formula/code-kaizen.rb
@@ -1,8 +1,8 @@
 class CodeKaizen < Formula
   desc "Smart hot-spots detection in codebases"
   homepage "https://github.com/asafkorem/code-kaizen"
-  url "https://github.com/asafkorem/code-kaizen/archive/archive/code-kaizen-1.1.19.tar.gz"
-  sha256 "5b9bb1fe8f7ebbeaea111deac1d0fe107aaf06e9643fa575855c50fec877f0cf"
+  url "https://github.com/asafkorem/code-kaizen/archive/archive/code-kaizen-1.1.20.tar.gz"
+  sha256 "089dfb35a44b0093f949a582704202fae8aa37ba036e575db62a27bd41afc529"
   license "MIT"
 
   def install
